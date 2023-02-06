d1b9d3317e065bd16d5c908fd7f4a970cdeaacf6
CindySun0309 <csun76@smith.edu>
2023-02-06 10:00:01 -0500

    write with help from tutors

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,27 @@
+from computer import *
+
 class ResaleShop:
 
     # What attributes will it need?
+    inventory = []
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
-        pass # You'll remove this when you fill out your constructor
+        pass 
 
     # What methods will you need?
+    def buy(self, new_computer):
+        self.inventory.append(new_computer)
+        # 1. call Computer(...) constructor to create a new Computer instance
+        # 2. call inventory.append(...) to add the new Computer instance to the inventory
+    
+    def sell(self, remove_computer):
+        self.inventory.remove(remove_computer)
+
+    def print_inventory(self):
+        print(self.inventory)
+
+
+    
+    
